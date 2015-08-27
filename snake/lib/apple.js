92350b41ac2ecf5cacd88976deefef8c78756201
Chas McMahon <cmcmahon2@fordham.edu>
2015-08-26 21:10:11 -0400

    removes extraneous files, modifies project links

--- snake/lib/apple.js
+++
@@ -1,9 +0,0 @@
-(function (root) {
-  var SnakeGame = root.SnakeGame = (root.SnakeGame || {});
-
-  var Apple = SnakeGame.Apple = function(coord, board){
-    this.coord = coord;
-    this.board = board;
-  }
-
-})(this);
